3873a57da2d2c2e22bf6ae709ca99c70db84b902
z4tz <z4tz87@gmail.com>
2015-04-08 00:35:57 +0200

    first

---
+++ chess board/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chess_board
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int row = 0; row < 8; row++)
+            {
+                for (int col = 0; col < 8; col++)
+                {
+                    if ((row+col)%2 ==0)
+                    {
+                        Console.Write('X');
+                    }
+                    else
+                    {
+                        Console.Write('O');
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
